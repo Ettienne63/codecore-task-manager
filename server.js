be290069a0743df45cd8fe8084f0c6cd19a07878
Ettienne Janse van Vuuren <ettienne.jvv@icloud.com>
2026-07-22 10:07:07 +0200

    initialize Project

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/",(req,res) => {
+    res.send("CodeCore Task Manager is running")
+})
+
+app.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}`)
+})
