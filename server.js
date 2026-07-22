96341e108f3b92a4dc974c2cd631e3c58e06d5dc
Ettienne Janse van Vuuren <ettienne.jvv@icloud.com>
2026-07-22 14:42:46 +0200

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
-require("dotenv").config();
+import "dotenv/config";
+import express from "express";
+import prisma from "./lib/prisma.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const express = require("express");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/",(req,res) => {
-    res.send("CodeCore Task Manager is running")
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req,res) => {
+    const tasks = await prisma.task.findMany()
+
+    res.render("tasks/index",{
+        pageTitle: "Tasks",
+        tasks
+    })
 })
+
+app.post("/tasks", async (req, res) => {
+    await prisma.task.create({
+        data: {
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            dueDate: req.body.dueDate
+                ? new Date(req.body.dueDate)
+                : null
+        }
+    });
+
+    res.redirect("/");
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`Server running at http://localhost:${port}`)
